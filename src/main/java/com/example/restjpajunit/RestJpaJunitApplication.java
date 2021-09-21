77bcdf8e5d87f92c63b53c84546c956480296df9
Ayan Pal <ayan.pal@live.com>
2021-09-21 21:57:46 +0530

    first commit

---
+++ src/main/java/com/example/restjpajunit/RestJpaJunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.restjpajunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJpaJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJpaJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restjpajunit/RestJpaJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restjpajunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJpaJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
